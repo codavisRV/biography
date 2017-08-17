05fc95566c56cdf00a3dcd013d394189dda5a91b
Courtney <codavis@redventures.com>
2017-08-17 14:12:24 -0400

    Fixes footer across all pages

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var nav = document.getElementById("desktop-nav");
-var hamburger = document.getElementById("hamburger-icon");
-var exit = document.getElementById("close-icon");
+
+//Nav Controls
+const nav = document.getElementById("desktop-nav");
+const hamburger = document.getElementById("hamburger-icon");
+const exit = document.getElementById("close-icon");
 hamburger.addEventListener("click", function() {
     nav.style.display = "block";
     exit.style.display = "block";
@@ -8,10 +10,26 @@ hamburger.addEventListener("click", function() {
 });
 
 exit.addEventListener("click", resetNav)
-nav.addEventListener("click", resetNav);
+nav.addEventListener("click", resetNav); //need to fix this. It also closes if you don't actually click a link
 
 function resetNav () {
     nav.style.display = "none";
     exit.style.display = "none";
     hamburger.style.display = "block";
 }
+
+//Form Submit
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", function () {
+    
+    var inputs = document.getElementsByTagName("input");
+    console.log(inputs);
+    return false; //not working?? 
+    
+});
+
+
+// gallery box toggle
+const galleryTab = document.getElementById("tab1");
+
+console.log(galleryTab);
